2e8677cc88ecb59287583b1470d504085545ecbf
Arsen Sydoryk <arsen.sydoryk@gmail.com>
2023-10-05 13:44:52 +0300

    [LAB-1] Implemented third subtask of first task

--- lab1/task1/client.py
+++ lab1/task1/client.py
@@ -1,12 +1,30 @@
 import socket
 
-server_data = ('localhost', 65_000)
+class Client:
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(server_data)
+    SERVER_HOST = "localhost"
+    SERVER_PORT = 65_000
 
-message = input("Plase enter message: ")
-client.sendall(message.encode('utf-8'))
-response = client.recv(1024)
-print("Received message from server: " + response.decode("utf-8"))
-client.close()
+    def __init__(self) -> None:
+        self.__client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.connect()
+    
+    def connect(self):
+        self.__client.connect((Client.SERVER_HOST, Client.SERVER_PORT))
+        self.run()
+    
+    def disconnect(self):
+        self.__client.close()
+
+    def run(self):
+        while True:
+            message = input("Please enter message: ")
+            self.__client.sendall(message.encode('utf-8'))
+            response = self.__client.recv(1024)
+            print("Received message from server: " + response.decode("utf-8"))
+            if(message == "Stop"):
+                break
+        self.disconnect()
+
+if(__name__ == "__main__"):
+    Client()
--- lab1/task1/server.py
+++ lab1/task1/server.py
@@ -2,24 +2,41 @@ import socket
 from datetime import datetime
 from time import sleep
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+class Server:
 
-host = 'localhost'
-port = 65_000
-server_socket.bind((host, port))
-server_socket.listen(5)
+    HOST = "localhost"
+    PORT = 65_000
 
-print("Ready...")
-client_socket, addr = server_socket.accept()
-message = client_socket.recv(1024)
-decoded_message = message.decode("utf-8")
-print("Received message \"" + decoded_message + "\" at " + str(datetime.now()))
+    def __init__(self) -> None:
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_socket.bind((Server.HOST, Server.PORT))
+        server_socket.listen(5)
+        self.__serverSocket = server_socket
+        print("Ready...")
+        self.run()
 
-sleep(5)
-received_bytes = client_socket.send(message)
-if(received_bytes == len(decoded_message)):
-    print("Everything's good")
-else:
-    print("Something went wrong...")
+    def run(self):
+        while True:
+            client_socket, addr = self.__serverSocket.accept()
+            print("Client " + str(addr) + " has connected! (" + str(datetime.now()) + ")")
+            while True:
+                message = self.getClientMessage(client_socket)
+                if(message == "Hello"):
+                    self.sendMessage(client_socket, "Hello!")
+                elif(message == "Stop"):
+                    self.sendMessage(client_socket, "Thank you for using our services!")
+                    break
+                else:
+                    self.sendMessage(client_socket, "Unknown command...")
+            print("Client " + str(addr) + " has disconnected! (" + str(datetime.now()) + ")")
+            client_socket.close()
 
-server_socket.close()
+
+    def sendMessage(self, client_socket: socket, message: str):
+        client_socket.send(message.encode('utf-8'))
+
+    def getClientMessage(self, client_socket: socket) -> str:
+        return client_socket.recv(1024).decode('utf-8')
+
+if(__name__ == "__main__"):
+    Server()
