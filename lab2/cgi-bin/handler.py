3e81757c9fd8f0d32992e71aef4d7be0a78ce18a
Arsen Sydoryk <arsen.sydoryk@gmail.com>
2023-10-10 23:01:59 +0300

    [LAB-2] Added selector

--- lab2/cgi-bin/handler.py
+++ lab2/cgi-bin/handler.py
@@ -17,16 +17,32 @@ right_answers = {
 }
 
 mark = 0
+question = 1
+print("<p> You answers: </p>")
 for key in key_set: 
     value = form_data.getvalue(key)
     right_answer = right_answers[key]
 
+    text = f"<p> {question}. "
     if type(right_answer) == list:
+        text += ",".join(value)
         points = 1 / len(right_answer)
-        for variant in right_answer:
-            if variant in value:
+        for variant in value:
+            if variant in right_answer:
                 mark += points
     else:
+        text += value
         mark += 1 if right_answer == value else 0
+    text += "</p>"
+    print(text)
 
-print(f"Total points: {mark}")
+seventh = form_data.getvalue('seventh')
+print(f"<p>Total points: {mark}</p>")
+
+if seventh == 'java':
+    if mark > 5:
+        print("Good job!")
+    else:
+        print("Not a good job...")
+else:
+    print("Good job!")
