726cbe2e8c84f4f5b5b5028931d671ff8a3af6df
Arsen Sydoryk <arsen.sydoryk@gmail.com>
2023-10-19 00:07:18 +0300

    [LAB-3] Added base pages

--- lab3/app.py
+++ lab3/app.py
@@ -1,11 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import os, datetime
 
 app = Flask(__name__)
 
+my_skills = ['Java', 'PHP', 'C++' 'Spring Boot', 'Laravel', 'PostgreSQL', 'MySQL',
+             'Elasticsearch', 'JavaScript', 'Angular', 'Python', 'Docker']
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@app.route("/")
+@app.route("/home")
+def home():
+    return render("home")
+@app.route("/about")
+def about():
+    return render("about")
+
+@app.route("/contacts")
+def contacts():
+    return render("contacts")
+
+@app.route("/skills")
+@app.route("/skills/<int:id>")
+def skills(id=None):
+    return render("skills")
+
+def render(template):
+    info = [os.name, datetime.datetime.now(), request.user_agent]
+    return render_template(template + ".html", data=info)
 
 
 if __name__ == '__main__':
