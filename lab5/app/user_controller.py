3c93b45562e2c78ec4ce6dfdec3b46bad1b496c3
Arsen Sydoryk <arsen.sydoryk@gmail.com>
2023-10-29 19:47:37 +0200

    [LAB-5] Used Flash messages for cookies

--- lab5/app/user_controller.py
+++ lab5/app/user_controller.py
@@ -62,9 +62,9 @@ def add_cookie():
     cookie_name = request.form.get('cookie_name')
     cookie_value = request.form.get('cookie_value')
     cookie_expires_at = request.form.get('expires_at')
-    session['cookie_created'] = f"You have successfully created a cookie {cookie_name}"
     response = make_response(redirect(url_for('info')))
     response.set_cookie(cookie_name, cookie_value, expires=cookie_expires_at)
+    flash(f"You have successfully created a cookie {cookie_name}", category="success")
     return response
 
 
@@ -72,11 +72,11 @@ def add_cookie():
 @app.route('/clear_cookie/<cookie_name>', methods=['GET'])
 def clear_cookie(cookie_name=None):
     if request.method == "POST":
-        session['cookie_deleted'] = f"You have successfully deleted all cookies"
         response = make_response(redirect(url_for('info')))
         for cookie in request.cookies.keys():
             if cookie != 'session':
                 response.delete_cookie(cookie)
+        flash("You have successfully deleted all cookies", category="success")
         return response
 
     if cookie_name is None:
@@ -84,9 +84,10 @@ def clear_cookie(cookie_name=None):
 
     if cookie_name not in request.cookies.keys():
         session['cookie_error'] = f"Undefined cookie {cookie_name}"
+        flash(f"Undefined cookie {cookie_name}", category="danger")
         return redirect(url_for('info'))
 
-    session['cookie_deleted'] = f"You have successfully deleted cookie {cookie_name}"
+    flash(f"You have successfully deleted cookie {cookie_name}", category="success")
     response = make_response(redirect(url_for('info')))
     response.delete_cookie(cookie_name)
     return response
