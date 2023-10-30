fe4b495cdef8f5a5d1a1af543c67e1a35a5a7339
Arsen Sydoryk <30925793+ArsenOOO7@users.noreply.github.com>
2023-10-31 00:26:35 +0200

    Lab5 (#7)
    * [LAB-5] Init
    
    * [LAB-5] Used WTF forms in login
    
    * [LAB-5] Used Flash messages for logout
    
    * [LAB-5] Used WTF forms for change password form
    
    * [LAB-5] Used Flash messages for cookies
    
    * [LAB-5] Fixed change password form with csrf token & remembered user login

---
+++ lab5/app/forms.py
@@ -0,0 +1,28 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired, Length
+
+
+class LoginForm(FlaskForm):
+    login = StringField("Login",
+                        render_kw={"placeholder": "Login..."},
+                        validators=[
+                            DataRequired(message="Login cannot be empty.")
+                        ])
+    password = PasswordField("Password",
+                             render_kw={"placeholder": "Password..."},
+                             validators=[
+                                 DataRequired(message="Password cannot be empty."),
+                                 Length(min=4, max=10)
+                             ])
+    remember = BooleanField("Remember", default=False)
+
+
+class ChangePassword(FlaskForm):
+    new_password = PasswordField("New Password",
+                                 render_kw={"placeholder": "New password..."},
+                                 validators=[
+                                     DataRequired(message="Password cannot be empty."),
+                                     Length(min=4, max=10)
+                                 ])
+    submit = SubmitField("Submit", render_kw={"class": "btn btn-primary mt-md-3"})
---
+++ lab5/app/user_controller.py
@@ -0,0 +1,115 @@
+from app import app
+from app.common.common import render
+from .forms import LoginForm, ChangePassword
+from flask import request, session, redirect, make_response, url_for, flash
+import json
+
+
+@app.route('/login', methods=['GET'])
+def login():
+    login_form = LoginForm()
+    if 'login_form_login_errors' in session:
+        login_form.login.errors = session.pop('login_form_login_errors')
+    if 'login_form_password_errors' in session:
+        login_form.password.errors = session.pop('login_form_password_errors')
+    if 'login_form_login_value' in session:
+        login_form.login.data = session.pop('login_form_login_value')
+    return render('user/login', login_form=login_form)
+
+
+@app.route('/login', methods=['POST'])
+def login_handle():
+    login_form = LoginForm()
+    session['login_form_login_value'] = login_form.login.data
+    if login_form.validate_on_submit():
+        with open('resources/users.json', 'r') as users:
+            login_value = login_form.login.data
+            password = login_form.password.data
+            json_data = json.load(users)
+            if json_data.get(login_value) is not None:
+                user_data = json_data[login_value]
+                if user_data['password'] == password:
+                    del user_data['password']
+                    if login_form.remember.data:
+                        session['user'] = user_data
+                        session['user']['login'] = login_value
+                        session.pop('login_form_login_value')
+                    flash("You successfully logged in.", category="success")
+                    return redirect(url_for("info"))
+            flash("Invalid credentials.", category="danger")
+            return redirect(url_for('login'))
+    session['login_form_login_errors'] = login_form.login.errors
+    session['login_form_password_errors'] = login_form.password.errors
+    return redirect(url_for('login'))
+
+
+@app.route('/info')
+def info():
+    if session.get('user') is None:
+        return redirect(url_for('login'))
+    cookies = request.cookies.items()
+    change_password_form = ChangePassword()
+    if 'form_cp_errors' in session:
+        change_password_form.new_password.errors = session.pop('form_cp_errors')
+    return render('user/info', cookies=cookies, change_password_form=change_password_form)
+
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    flash('You have successfully logged out!', category="success")
+    return redirect(url_for('login'))
+
+
+@app.route("/cookie", methods=['POST'])
+def add_cookie():
+    cookie_name = request.form.get('cookie_name')
+    cookie_value = request.form.get('cookie_value')
+    cookie_expires_at = request.form.get('expires_at')
+    response = make_response(redirect(url_for('info')))
+    response.set_cookie(cookie_name, cookie_value, expires=cookie_expires_at)
+    flash(f"You have successfully created a cookie {cookie_name}", category="success")
+    return response
+
+
+@app.route('/clear_cookie', methods=['POST'])
+@app.route('/clear_cookie/<cookie_name>', methods=['GET'])
+def clear_cookie(cookie_name=None):
+    if request.method == "POST":
+        response = make_response(redirect(url_for('info')))
+        for cookie in request.cookies.keys():
+            if cookie != 'session':
+                response.delete_cookie(cookie)
+        flash("You have successfully deleted all cookies", category="success")
+        return response
+
+    if cookie_name is None:
+        return redirect(url_for('info'))
+
+    if cookie_name not in request.cookies.keys():
+        session['cookie_error'] = f"Undefined cookie {cookie_name}"
+        flash(f"Undefined cookie {cookie_name}", category="danger")
+        return redirect(url_for('info'))
+
+    flash(f"You have successfully deleted cookie {cookie_name}", category="success")
+    response = make_response(redirect(url_for('info')))
+    response.delete_cookie(cookie_name)
+    return response
+
+
+@app.route('/change_password', methods=['POST'])
+def change_password():
+    change_password_form = ChangePassword()
+    if change_password_form.validate_on_submit():
+        new_password = change_password_form.new_password.data
+        user_login = session['user']['login']
+        with open('resources/users.json', 'r') as file:
+            data = json.load(file)
+        data[user_login]['password'] = new_password
+
+        with open('resources/users.json', 'w') as file:
+            json.dump(data, file)
+        flash("You successfully changed your password!", category="success")
+        return redirect(url_for('info'))
+    session['form_cp_errors'] = change_password_form.new_password.errors
+    return redirect(url_for('info'))
