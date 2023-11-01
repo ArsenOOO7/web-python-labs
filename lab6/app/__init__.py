8f08668fffce4e6b62d359ef56b6812f3598ca36
Arsen Sydoryk <arsen.sydoryk@gmail.com>
2023-11-01 23:23:57 +0200

    [LAB-6] Connected Flask SQLAlchemy and created Task entity

--- lab6/app/__init__.py
+++ lab6/app/__init__.py
@@ -1,7 +1,16 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.secret_key = b"secret"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///laboratory_work.db'
+data_base = SQLAlchemy(app)
+
+from app.domain import Task
+
+with app.app_context():
+    data_base.create_all()
 
 from app import main_controller
+from app import task_controller
 from app import user_controller
---
+++ lab6/app/domain/Task.py
@@ -0,0 +1,22 @@
+from enum import Enum
+
+from app import data_base
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import Integer, String, Enum as SQLEnum
+
+
+class Status(Enum):
+    TODO = 'ToDo'
+    IN_PROGRESS = 'In progress'
+    DONE = 'Done'
+
+    @staticmethod
+    def get_dropdown_values():
+        return [(status.name, status.value) for status in Status]
+
+
+class Task(data_base.Model):
+    __tablename__ = 'task'
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String, unique=True, nullable=False)
+    status: Mapped[str] = mapped_column(SQLEnum(Status), nullable=False, default=Status.TODO)
