da4e0e232122e8fd9456776a8167be43e1f86a74
Arsen Sydoryk <30925793+ArsenOOO7@users.noreply.github.com>
2023-11-02 19:35:43 +0200

    Lab6 (#8)
    * [LAB-6] Init
    
    * [LAB-6] Added new option to menu
    
    * [LAB-6] Reformatted
    
    * [LAB-6] Connected Flask SQLAlchemy and created Task entity
    
    * [LAB-6] Created controller and forms for Task
    
    * [LAB-6] Added html files
    
    * [LAB-6] Removed db file
    
    * [LAB-6] Migration init
    
    * [LAB-6] Added description column to Task
    
    * [LAB-6] Added description in forms and controller
    
    * [LAB-6] Added requirements
    
    * [LAB-6] Added flash messages
    
    * [LAB-6] Refactored authorization

---
+++ lab6/app/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+app.secret_key = b"secret"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///laboratory_work.db'
+data_base = SQLAlchemy(app)
+
+from app.domain import Task
+
+migrate = Migrate(app, data_base)
+
+from app import main_controller
+from app import task_controller
+from app import user_controller
