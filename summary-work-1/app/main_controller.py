51cdadd89b7bd632331f58ea2b9841c6936b3f88
Arsen Sydoryk <30925793+ArsenOOO7@users.noreply.github.com>
2023-11-08 20:47:28 +0200

    Summary work 1 (#9)
    * [SUMMARY-1] Init project
    
    * [SUMMARY-1] Migration init
    
    * [SUMMARY-1] Added feedback entity
    
    * [SUMMARY-1] Added feedback form and table
    
    * [SUMMARY-1] Cleared up html
    
    * [SUMMARY-1] Added new columns to Feedback
    
    * [SUMMARY-1] Added flash messages and option for deleting feedbacks

---
+++ summary-work-1/app/main_controller.py
@@ -0,0 +1,34 @@
+from app import app
+from app.common.common import render
+
+my_skills = ['Java', 'PHP', 'C++', 'Spring Boot', 'Laravel', 'PostgreSQL', 'MySQL',
+             'Elasticsearch', 'JavaScript', 'Angular', 'Python', 'Docker']
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render("home")
+
+
+@app.route("/about")
+def about():
+    return render("about")
+
+
+@app.route("/contacts")
+def contacts():
+    return render("contacts")
+
+
+@app.route("/skills")
+@app.route("/skills/<id>")
+def skills(id=None):
+    skill_name = None
+    if id:
+        try:
+            skill_name = id if my_skills[my_skills.index(id)] == id else 'Undefined'
+        except ValueError:
+            skill_name = 'Undefined!'
+    return render("skills",
+                  skills=my_skills, skill_name=skill_name, total_len=len(my_skills))
