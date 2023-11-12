23dadf0ecec6dfa8db2b5321541f41247f36127f
Arsen Sydoryk <30925793+ArsenOOO7@users.noreply.github.com>
2023-11-12 23:53:08 +0200

    Laboratory #9 (#12)
    * [LAB-9] Init
    
    * [LAB-9] Init migration
    
    * [LAB-9] Added account page
    
    * [LAB-9] Added update user details form to account page
    
    * [LAB-9] Added last seen and about me columns
    
    * [LAB-9] Added logic for user details update
    
    * [LAB-9] Added flash message about updating account details
    
    * [LAB-9] Implemented last_seen logic
    
    * [LAB-9] Changed image file field
    
    * [LAB-9] Avatar field migrations
    
    * [LAB-9] Added  avatar logic
    
    * [LAB-9] Added config
    
    * [LAB-9] Added old password field to change password form
    
    * [LAB-9] Fixed bug

---
+++ lab9/app/domain/User.py
@@ -0,0 +1,49 @@
+import datetime
+
+from flask_bcrypt import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from sqlalchemy import Integer, String, DateTime
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.sql import func
+
+from app import data_base, login_manager
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class User(data_base.Model, UserMixin):
+    __tablename__ = "users"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    username: Mapped[str] = mapped_column(String(20), unique=True, nullable=False, index=True)
+    first_name: Mapped[str] = mapped_column(String(20), nullable=False)
+    last_name: Mapped[str] = mapped_column(String(20), nullable=False)
+    password: Mapped[str] = mapped_column(String(256), nullable=False)
+    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False, index=True)
+    birth_date: Mapped[str] = mapped_column(String(30), nullable=True)
+    avatar_file: Mapped[str] = mapped_column(String(40), nullable=False, server_default='default.jpg', primary_key=False)
+    about_me: Mapped[str] = mapped_column(String(500), nullable=True)
+    last_seen: Mapped[datetime.datetime] = mapped_column(DateTime, server_default=func.now(), nullable=False)
+
+    @property
+    def user_password(self):
+        raise AttributeError('You cannot read password')
+
+    @user_password.setter
+    def user_password(self, new_password):
+        self.password = generate_password_hash(new_password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password, password)
+
+    def create_user_details(self):
+        return {
+            'login': self.username,
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'birth_date': self.birth_date,
+            'email': self.email,
+            'last_seen': self.last_seen.strftime('%m/%d/%y %H:%M')
+        }
---
+++ lab9/app/forms.py
@@ -0,0 +1,169 @@
+from flask_login import current_user
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, SelectField, TextAreaField, RadioField, \
+    EmailField, DateField, FileField
+from wtforms.validators import DataRequired, Length, EqualTo, ValidationError, Regexp
+
+from app.common import to_readable
+from app.domain.Feedback import Satisfaction
+from app.domain.Task import Status
+from app.domain.User import User
+
+
+class RegisterForm(FlaskForm):
+    username = StringField("Username",
+                           render_kw={'placeholder': 'Username...'},
+                           validators=[
+                               DataRequired(message="Username is required."),
+                               Regexp("^[A-Za-z0-9\\._\\-]*$", 0,
+                                      'Username should have letters, numbers, underscores, dots')
+                           ])
+    first_name = StringField("First name",
+                             render_kw={'placeholder': 'First name...'},
+                             validators=[
+                                 DataRequired(message="First name is required."),
+                                 Regexp('^[A-Z]{1}[a-z]*$', 0,
+                                        'First name should have contain only letters with the first one capitalized')
+                             ])
+    last_name = StringField("Last name",
+                            render_kw={'placeholder': 'Last name...'},
+                            validators=[
+                                DataRequired(message="Last name is required."),
+                                Regexp('^[A-Z]{1}[a-z]*$', 0,
+                                       'Last name should have contain only letters with the first one capitalized')
+                            ])
+    email = EmailField('Email',
+                       render_kw={'placeholder': 'Email...'},
+                       validators=[
+                           DataRequired(message="Email is required.")
+                       ])
+    password = PasswordField('Password',
+                             render_kw={'placeholder': 'Password...'},
+                             validators=[
+                                 DataRequired(message="Last name is required."),
+                                 Regexp('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d]{8,}$', 0,
+                                        'Passwords must contain minimum eight characters, at least one uppercase '
+                                        'letter, one lowercase letter and one number!')
+                             ])
+    confirm_password = PasswordField('Confirm password',
+                                     render_kw={'placeholder': 'Confirm password...'},
+                                     validators=[
+                                         DataRequired(message="Confirm password is required."),
+                                         EqualTo('password')
+                                     ])
+    birth_date = DateField('Birth date',
+                           render_kw={'placeholder': 'Birth date...'})
+    user_image = FileField('Avatar',
+                           render_kw={'placeholder': 'Avatar...', 'accept': '.jpg, .jpeg, .png'})
+
+    def validate_username(self, field):
+        if User.query.filter(User.username == field.data).first():
+            raise ValidationError('Username already exists.')
+
+    def validate_email(self, field):
+        if User.query.filter(User.email == field.data).first():
+            raise ValidationError('Email already exists.')
+
+
+class UpdateUserForm(FlaskForm):
+    username = StringField("Username",
+                           render_kw={'placeholder': 'Username...'},
+                           validators=[
+                               DataRequired(message="Username is required."),
+                               Regexp("^[A-Za-z0-9\\._\\-]*$", 0,
+                                      'Username should have letters, numbers, underscores, dots')
+                           ])
+    first_name = StringField("First name",
+                             render_kw={'placeholder': 'First name...'},
+                             validators=[
+                                 DataRequired(message="First name is required."),
+                                 Regexp('^[A-Z]{1}[a-z]*$', 0,
+                                        'First name should have contain only letters with the first one capitalized')
+                             ])
+    last_name = StringField("Last name",
+                            render_kw={'placeholder': 'Last name...'},
+                            validators=[
+                                DataRequired(message="Last name is required."),
+                                Regexp('^[A-Z]{1}[a-z]*$', 0,
+                                       'Last name should have contain only letters with the first one capitalized')
+                            ])
+    email = EmailField('Email',
+                       render_kw={'placeholder': 'Email...'},
+                       validators=[
+                           DataRequired(message="Email is required.")
+                       ])
+    birth_date = DateField('Birth date',
+                           render_kw={'placeholder': 'Birth date...'})
+    user_image = FileField('Avatar',
+                           render_kw={'placeholder': 'Avatar...', 'accept': '.jpg, .jpeg, .png'})
+    about_me = TextAreaField('About me',
+                             render_kw={'placeholder': 'About me...'})
+
+    def validate_username(self, field):
+        if current_user.username != field.data and User.query.filter(User.username == field.data).first():
+            raise ValidationError('Username already exists.')
+
+    def validate_email(self, field):
+        if current_user.email != field.data and User.query.filter(User.email == field.data).first():
+            raise ValidationError('Email already exists.')
+
+
+class LoginForm(FlaskForm):
+    login = StringField("Login",
+                        render_kw={"placeholder": "Login..."},
+                        validators=[
+                            DataRequired(message="Login cannot be empty.")
+                        ])
+    password = PasswordField("Password",
+                             render_kw={"placeholder": "Password..."},
+                             validators=[
+                                 DataRequired(message="Password cannot be empty.")
+                             ])
+    remember = BooleanField("Remember", default=False)
+
+
+class ChangePassword(FlaskForm):
+    old_password = PasswordField('Old password',
+                                 render_kw={'placeholder': 'Old password...'})
+    new_password = PasswordField("New Password",
+                                 render_kw={"placeholder": "New password..."},
+                                 validators=[
+                                     DataRequired(message="Password cannot be empty."),
+                                     Regexp('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d]{8,}$', 0,
+                                            'Passwords must contain minimum eight characters, at least one uppercase '
+                                            'letter, one lowercase letter and one number!')
+                                 ])
+    submit = SubmitField("Submit", render_kw={"class": "btn btn-primary mt-md-3"})
+
+
+class AddTask(FlaskForm):
+    name = StringField("Name",
+                       render_kw={"placeholder": "Name..."},
+                       validators=[
+                           DataRequired(message="Task name is required.")
+                       ])
+    description = TextAreaField("Description", render_kw={"placeholder": "Description..."})
+
+
+class UpdateTask(FlaskForm):
+    name = StringField("Name",
+                       render_kw={"placeholder": "Name..."},
+                       validators=[
+                           DataRequired(message="Task name is required.")
+                       ])
+    description = TextAreaField("Description", render_kw={"placeholder": "Description..."})
+    status = SelectField("Status", choices=Status.get_dropdown_values())
+
+
+class AddFeedback(FlaskForm):
+    feedback = TextAreaField("Feedback",
+                             render_kw={'placeholder': 'Give your feedback...'},
+                             validators=[
+                                 DataRequired(message="Feedback is required.")
+                             ])
+    satisfaction = RadioField("Satisfaction",
+                              choices=[(satisfaction.name, to_readable(satisfaction.value)) for satisfaction in
+                                       Satisfaction],
+                              validators=[
+                                  DataRequired(message="Select your level of satisfaction.")
+                              ])
