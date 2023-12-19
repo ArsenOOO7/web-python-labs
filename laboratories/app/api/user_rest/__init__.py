ab1bd7e2eefc04248270ab968b09fded9b4a3a6b
Arsen Sydoryk <arsen.sydoryk@gmail.com>
2023-12-19 03:14:22 +0200

    [LAB-15] Fixed error handling

--- laboratories/app/api/user_rest/__init__.py
+++ laboratories/app/api/user_rest/__init__.py
@@ -5,12 +5,12 @@ from marshmallow import ValidationError
 from .controller import UserRestController, UserListRestController
 
 user_rest_bp = Blueprint('user_rest_bp', __name__)
-api = Api(user_rest_bp, errors=user_rest_bp.errorhandler)
+api = Api(user_rest_bp, errors=user_rest_bp.app_errorhandler)
 
 api.add_resource(UserRestController, '', '/', '/<int:id>')
 api.add_resource(UserListRestController, '/list')
 
 
-@user_rest_bp.errorhandler(ValidationError)
+@user_rest_bp.app_errorhandler(ValidationError)
 def handle_marshmallow_error(e):
     return jsonify(e.messages), 400
