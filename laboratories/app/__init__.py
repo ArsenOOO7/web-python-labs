d8509b136d413e18f26587de1b97651764bfa805
Arsen Sydoryk <arsen.sydoryk@gmail.com>
2024-01-18 02:21:01 +0200

    [HOTFIX] Changed sequence

--- laboratories/app/__init__.py
+++ laboratories/app/__init__.py
@@ -1,9 +1,8 @@
-from flask import Flask, url_for
+from flask import Flask
 from flask_login import LoginManager
 from flask_marshmallow import Marshmallow
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
-from flask_swagger_ui import get_swaggerui_blueprint
 
 from app.swagger import swagger_bp
 from app.util.jwt_utils import JwtUtils
